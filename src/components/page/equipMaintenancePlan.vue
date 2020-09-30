ce6baa49589515a7c71d01837189f8df0e87d345
zengxiaochao <zengxiaochao666@gmail.com>
2020-09-30 17:14:00 +0800

    V2.9设备管理基本框架搭建完成

--- src/components/page/equipMaintenancePlan.vue
+++ src/components/page/equipMaintenancePlan.vue
@@ -36,11 +36,10 @@
             >
                 <el-table-column type="selection" width="55" align="center"></el-table-column>
                     <el-table-column prop="equipType" label="设备类型"></el-table-column>
-                    <el-table-column prop="cycle" label="保养周期：周，月，年"></el-table-column>
+                    <el-table-column prop="cycle" label="保养周期"></el-table-column>
                     <el-table-column prop="warnTime" label="预警时间"></el-table-column>
                     <el-table-column prop="maintenance" label="保养内容"></el-table-column>
-                    <el-table-column prop="userName" label="保养人姓名"></el-table-column>
-                    <el-table-column prop="userId" label="保养人"></el-table-column>
+                    <el-table-column prop="userName" label="保养人"></el-table-column>
 
 
 
@@ -99,18 +98,26 @@
         <!-- 编辑弹出框 -->
         <el-dialog title="编辑" :visible.sync="editVisible" width="30%">
             <el-form ref="form" :model="form" label-width="95px">
-                    <el-form-item label="id"><el-input v-model="form.id"></el-input></el-form-item>
-                    <el-form-item label="设备id"><el-input v-model="form.equipId"></el-input></el-form-item>
-                    <el-form-item label="设备编号"><el-input v-model="form.equipNo"></el-input></el-form-item>
-                    <el-form-item label="设备类型"><el-input v-model="form.equipType"></el-input></el-form-item>
-                    <el-form-item label="所处产线"><el-input v-model="form.equipLoc"></el-input></el-form-item>
-                    <el-form-item label="故障描述"><el-input v-model="form.faultDesc"></el-input></el-form-item>
-                    <el-form-item label="状态"><el-input v-model="form.status"></el-input></el-form-item>
-                    <el-form-item label="上报人姓名"><el-input v-model="form.reportPerson"></el-input></el-form-item>
-                    <el-form-item label="维修工人"><el-input v-model="form.maintenanceWorker"></el-input></el-form-item>
-                    <el-form-item label="备注"><el-input v-model="form.remarks"></el-input></el-form-item>
-                    <el-form-item label="创建人"><el-input v-model="form.createBy"></el-input></el-form-item>
-                    <el-form-item label="更新人"><el-input v-model="form.updateBy"></el-input></el-form-item>
+                <el-form-item label="编号" v-show="false"><el-input :disabled="true" v-model="form.id"></el-input></el-form-item>
+                <el-form-item label="设备类型" prop="equipType">
+                    <el-select v-model="form.equipType" placeholder="请选择设备类型">
+                        <el-option key="1" label="电子秤" value="0001"></el-option>
+                        <el-option key="2" label="读卡器" value="0002"></el-option>
+                        <el-option key="3" label="条码打印机" value="0003"></el-option>
+                        <el-option key="4" label="安卓PAD" value="0004"></el-option>
+                        <el-option key="5" label="红外对射枪" value="0005"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="保养周期" prop="cycle">
+                    <el-select v-model="form.cycle" placeholder="请选择保养周期">
+                        <el-option key="1" label="周" value="week"></el-option>
+                        <el-option key="2" label="月" value="mouth"></el-option>
+                        <el-option key="3" label="年" value="year"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="预警时间"><el-input v-model="form.warnTime"></el-input></el-form-item>
+                <el-form-item label="保养人姓名"><el-input v-model="form.userName"></el-input></el-form-item>
+                <el-form-item label="保养内容"><el-input type="textarea"  v-model="form.maintenance"></el-input></el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="editVisible = false">取 消</el-button>
@@ -120,17 +127,26 @@
         <!-- 编辑添加框 -->
         <el-dialog title="添加" :visible.sync="addVisible" width="30%">
             <el-form ref="form" :model="form" label-width="95px">
-                <el-form-item label="设备id"><el-input v-model="form.equipId"></el-input></el-form-item>
-                <el-form-item label="设备编号"><el-input v-model="form.equipNo"></el-input></el-form-item>
-                <el-form-item label="设备类型"><el-input v-model="form.equipType"></el-input></el-form-item>
-                <el-form-item label="所处产线"><el-input v-model="form.equipLoc"></el-input></el-form-item>
-                <el-form-item label="故障描述"><el-input v-model="form.faultDesc"></el-input></el-form-item>
-                <el-form-item label="状态"><el-input v-model="form.status"></el-input></el-form-item>
-                <el-form-item label="上报人姓名"><el-input v-model="form.reportPerson"></el-input></el-form-item>
-                <el-form-item label="维修工人"><el-input v-model="form.maintenanceWorker"></el-input></el-form-item>
-                <el-form-item label="备注"><el-input v-model="form.remarks"></el-input></el-form-item>
-                <el-form-item label="创建人"><el-input v-model="form.createBy"></el-input></el-form-item>
-                <el-form-item label="更新人"><el-input v-model="form.updateBy"></el-input></el-form-item>
+                <el-form-item label="编号" v-show="false"><el-input :disabled="true" v-model="form.id"></el-input></el-form-item>
+                <el-form-item label="设备类型" prop="equipType">
+                    <el-select v-model="form.equipType" placeholder="请选择设备类型">
+                        <el-option key="1" label="电子秤" value="0001"></el-option>
+                        <el-option key="2" label="读卡器" value="0002"></el-option>
+                        <el-option key="3" label="条码打印机" value="0003"></el-option>
+                        <el-option key="4" label="安卓PAD" value="0004"></el-option>
+                        <el-option key="5" label="红外对射枪" value="0005"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="保养周期" prop="cycle">
+                    <el-select v-model="form.cycle" placeholder="请选择保养周期">
+                        <el-option key="1" label="周" value="week"></el-option>
+                        <el-option key="2" label="月" value="mouth"></el-option>
+                        <el-option key="3" label="年" value="year"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="预警时间"><el-input v-model="form.warnTime"></el-input></el-form-item>
+                <el-form-item label="保养人姓名"><el-input v-model="form.userName"></el-input></el-form-item>
+                <el-form-item label="保养内容"><el-input type="textarea"  v-model="form.maintenance"></el-input></el-form-item>
             </el-form>
             <span slot="footer" class="dialog-footer">
                 <el-button @click="addVisible = false">取 消</el-button>
@@ -141,7 +157,6 @@
 </template>
 
 <script>
-import { fetchData } from '../../api/index';
 export default {
     name: 'equipMaintenancePlan',
     data() {
@@ -175,8 +190,9 @@ export default {
         },
         // 触发搜索按钮
         handleSearch() {
-            this.$set(this.query, 'pageIndex', 1);
-            this.getData();
+            this.$axios.get('/api/equipMaintenancePlan/selectByType?equip_type='+this.query.address).then(res =>{
+                this.tableData = res.data;
+            })
         },
         // 删除操作
         handleDelete(index, row) {
@@ -186,7 +202,7 @@ export default {
             })
                 .then(() => {
                     this.tableData.splice(index, 1);
-                    this.$axios.get('/api/equipFaultReport/deleteById?id='+row.id).then(res=>{
+                    this.$axios.get('/api/equipMaintenancePlan/delete?id='+row.id).then(res=>{
                         this.$message.success("删除成功");
                     })
                 })
@@ -202,7 +218,7 @@ export default {
             this.delList = this.delList.concat(this.multipleSelection);
             for (let i = 0; i < length; i++) {
                 str += this.multipleSelection[i].id + ' ';
-                this.$axios.get('/api/equipFaultReport/deleteById?id='+this.multipleSelection[i].id).then(res=>{
+                this.$axios.get('/api/equipMaintenancePlan/delete?id='+this.multipleSelection[i].id).then(res=>{
                 })
             }
             this.$message.success(`删除了${str}`);
@@ -224,7 +240,7 @@ export default {
             console.log(this.form)
 
             this.editVisible = false;
-            this.$axios.post('/api/equipFaultReport/edit',this.form).then(res=>{
+            this.$axios.post('/api/equipMaintenancePlan/edit',this.form).then(res=>{
                 this.$message.success(`修改第 ${this.idx + 1} 行成功`);
             })
             this.$set(this.tableData, this.idx, this.form);
@@ -234,7 +250,7 @@ export default {
             console.log(this.form)
 
             this.addVisible = false;
-            this.$axios.post('/api/equipFaultReport/add',this.form).then(res=>{
+            this.$axios.post('/api/equipMaintenancePlan/add',this.form).then(res=>{
                 this.$message.success(`添加成功`);
             })
             this.getData();
--- src/components/page/equipRepairPerson.vue
+++ src/components/page/equipRepairPerson.vue
@@ -3,11 +3,35 @@
         <div class="crumbs">
             <el-breadcrumb separator="/">
                 <el-breadcrumb-item>
-                    <i class="el-icon-lx-cascades"></i> 设备维修报告
+                    <i class="el-icon-lx-cascades"></i> 设备报修派工记录
                 </el-breadcrumb-item>
             </el-breadcrumb>
         </div>
         <div class="container">
+            <div class="handle-box">
+
+                <el-select v-model="query.address1" placeholder="设备类型" class="handle-select mr10">
+                    <el-option key="1" label="" value=""></el-option>
+                    <el-option key="2" label="电子秤" value="0001"></el-option>
+                    <el-option key="3" label="读卡器" value="0002"></el-option>
+                    <el-option key="4" label="条码打印机" value="0003"></el-option>
+                    <el-option key="5" label="安卓PAD" value="0004"></el-option>
+                    <el-option key="6" label="红外对射枪" value="0005"></el-option>
+                </el-select>
+                <el-select v-model="query.address2" placeholder="设备位置" class="handle-select mr10">
+                    <el-option key="1" label="" value=""></el-option>
+                    <el-option key="2" label="ML001" value="0001"></el-option>
+                    <el-option key="3" label="ML002" value="0002"></el-option>
+                    <el-option key="4" label="ML003" value="0003"></el-option>
+                </el-select>
+                <el-select v-model="query.address3" placeholder="维修状态" class="handle-select mr10">
+                    <el-option key="1" label="" value=""></el-option>
+                    <el-option key="2" label="未派工" value="0001"></el-option>
+                    <el-option key="3" label="维修" value="0002"></el-option>
+                    <el-option key="4" label="完工" value="0003"></el-option>
+                </el-select>
+                <el-button type="primary" icon="el-icon-search" @click="handleSearch">搜索</el-button>
+            </div>
             <el-table
                 :data="tableData"
                 border
@@ -16,33 +40,73 @@
                 header-cell-class-name="table-header"
                 @selection-change="handleSelectionChange"
             >
-                <el-table-column type="selection" width="55" align="center"></el-table-column>
-                    <el-table-column prop="id" label="编号"></el-table-column>
-                    <el-table-column prop="mid" label="维修记录Id"></el-table-column>
-                    <el-table-column prop="faultType" label="故障类型"></el-table-column>
-                    <el-table-column prop="faultReason" label="故障原因"></el-table-column>
+                <el-table-column width="55" align="center"></el-table-column>
+                    <el-table-column prop="equipNo" label="设备编号"></el-table-column>
+                    <el-table-column prop="equipType" label="设备类型"></el-table-column>
+                    <el-table-column prop="equipLoc" label="所处产线"></el-table-column>
                     <el-table-column prop="faultDesc" label="故障描述"></el-table-column>
-                    <el-table-column prop="faultImgs" label="故障图片"></el-table-column>
-                    <el-table-column prop="delFlag" label="删除标记"></el-table-column>
-                    <el-table-column prop="remarks" label="备注"></el-table-column>
-                    <el-table-column prop="createBy" label="创建人"></el-table-column>
+                    <el-table-column prop="reportPerson" label="上报人姓名"></el-table-column>
                     <el-table-column prop="createDate" label="创建时间"></el-table-column>
-                    <el-table-column prop="updateBy" label="更新人"></el-table-column>
-                    <el-table-column prop="updateDate" label="更新时间"></el-table-column>
-
+                    <el-table-column prop="status" label="状态"></el-table-column>
+                    <el-table-column label="操作" width="180" align="center">
+                        <template slot-scope="scope">
+                            <el-button
+                                    type="text"
+                                    icon="el-icon-edit"
+                                    @click="handleEdit(scope.$index, scope.row)"
+                            >派工</el-button>
+                        </template>
+                    </el-table-column>
             </el-table>
 
         </div>
+        <!-- 编辑弹出框 -->
+        <el-dialog title="派工" :visible.sync="editVisible" width="30%">
+            <el-form ref="form" :model="form" label-width="95px">
+                <el-form-item label="编号" v-show="false"><el-input :disabled="true" v-model="form.id"></el-input></el-form-item>
+                <el-form-item label="用户名" v-show="false"><el-input :disabled="true" v-model="form.reportPerson"></el-input></el-form-item>
+                <el-form-item label="设备编号"><el-input :disabled="true" v-model="form.equipNo"></el-input></el-form-item>
+                <el-form-item label="设备类型" prop="equipType">
+                    <el-select v-model="form.equipType" placeholder="请选择设备类型" :disabled="true">
+                        <el-option key="1" label="电子秤" value="0001"></el-option>
+                        <el-option key="2" label="读卡器" value="0002"></el-option>
+                        <el-option key="3" label="条码打印机" value="0003"></el-option>
+                        <el-option key="4" label="安卓PAD" value="0004"></el-option>
+                        <el-option key="5" label="红外对射枪" value="0005"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="设备位置" prop="equipLoc">
+                    <el-select v-model="form.equipType" placeholder="请选择设备类型" :disabled="true">
+                        <el-option key="1" label="电子秤" value="0001"></el-option>
+                        <el-option key="2" label="读卡器" value="0002"></el-option>
+                        <el-option key="3" label="条码打印机" value="0003"></el-option>
+                        <el-option key="4" label="安卓PAD" value="0004"></el-option>
+                        <el-option key="5" label="红外对射枪" value="0005"></el-option>
+                    </el-select>
+                </el-form-item>
+                <el-form-item label="故障描述"><el-input :disabled="true" type="textarea"  v-model="form.faultDesc"></el-input></el-form-item>
+                <el-form-item label="保养人"><el-input v-model="form.maintenanceWorker"></el-input></el-form-item>
+            </el-form>
+            <span slot="footer" class="dialog-footer">
+                <el-button @click="editVisible = false">取 消</el-button>
+                <el-button type="primary" @click="saveEdit">确 定</el-button>
+            </span>
+        </el-dialog>
     </div>
 </template>
 
 <script>
-import { fetchData2 } from '../../api/index';
-import request from "../../utils/request";
 export default {
     name: 'equipRepairPerson',
     data() {
         return {
+            query: {
+                address1: '',
+                address2: '',
+                address3: '',
+                pageIndex: 0,
+                pageSize: 50
+            },
             tableData: [],
             multipleSelection: [],
             delList: [],
@@ -58,9 +122,42 @@ export default {
     },
     methods: {
         getData2() {
-            this.$axios.get('/api/equipRepair/selectAll').then(res =>{
-                this.tableData = res;
+            this.$axios.get('/api/equipFaultReport/selectAll').then(res =>{
+                this.tableData = res.data;
             })
+            this.form.reportPerson = JSON.parse(localStorage.getItem("userInfo")).name;
+        },
+        handleSearch() {
+            console.log('/api/equipFaultReport/select?equip_type='+this.query.address1+'&equip_loc='+this.query.address2+'&status='+this.query.address3);
+            this.$axios.get('/api/equipFaultReport/select?equip_type='+this.query.address1+'&equip_loc='+this.query.address2+'&status='+this.query.address3).then(res =>{
+
+                this.tableData = res.data;
+                this.toString();
+            })
+
+        },
+        // 编辑操作
+        handleEdit(index, row) {
+            this.idx = index;
+            this.form = row;
+            console.log(this.form)
+            if(row.status=='0003')
+            {
+                this.$message.error(`维修报告已提交，不能派工`);
+            }
+            else {
+                this.editVisible = true;
+            }
+        },
+        // 保存编辑
+        saveEdit() {
+            console.log(this.form)
+            this.editVisible = false;
+            this.form.status='0002';
+            this.$axios.post('/api/equipFaultReport/edit',this.form).then(res=>{
+                this.$message.success(`派工第 ${this.idx + 1} 行成功`);
+            })
+            this.$set(this.tableData, this.idx, this.form);
         }
     }
 };
